9ed6e73584f7270b0c6599569cc648332f0e9827
Rangers <447192998@qq.com>
2020-02-27 23:37:11 +0800

    update animation and code

--- manimlib/animation/animation.py
+++ manimlib/animation/animation.py
@@ -25,6 +25,8 @@ class Animation(object):
         # with lagged start times
         "lag_ratio": DEFAULT_ANIMATION_LAG_RATIO,
         "suspend_mobject_updating": True,
+        "submobject_mode": "None",    # 新增的
+        "lag_factor": 2,              # 新增的  
     }
 
     def __init__(self, mobject, **kwargs):
@@ -127,14 +129,37 @@ class Animation(object):
         # Typically ipmlemented by subclass
         pass
 
+    # def get_sub_alpha(self, alpha, index, num_submobjects):
+    #     # TODO, make this more understanable, and/or combine
+    #     # its functionality with AnimationGroup's method
+    #     # build_animations_with_timings
+    #     lag_ratio = self.lag_ratio
+    #     full_length = (num_submobjects - 1) * lag_ratio + 1
+    #     value = alpha * full_length
+    #     lower = index * lag_ratio
+    #     return np.clip((value - lower), 0, 1)
+
+    # enable submobject_mode
     def get_sub_alpha(self, alpha, index, num_submobjects):
-        # TODO, make this more understanable, and/or combine
-        # its functionality with AnimationGroup's method
-        # build_animations_with_timings
+    # TODO, make this more understanable, and/or combine
+    # its functionality with AnimationGroup's method
+    # build_animations_with_timings
         lag_ratio = self.lag_ratio
         full_length = (num_submobjects - 1) * lag_ratio + 1
         value = alpha * full_length
         lower = index * lag_ratio
+        if self.submobject_mode in ["lagged_start", "smoothed_lagged_start"]:
+            prop = float(index) / num_submobjects
+            if self.submobject_mode is "smoothed_lagged_start":
+                prop = smooth(prop)
+            lf = self.lag_factor
+            return np.clip(lf * alpha - (lf - 1) * prop, 0, 1)
+        elif self.submobject_mode == "one_at_a_time":
+            lower = float(index) / num_submobjects
+            upper = float(index + 1) / num_submobjects
+            return np.clip((alpha - lower) / (upper - lower), 0, 1)
+        elif self.submobject_mode == "all_at_once":
+            return alpha
         return np.clip((value - lower), 0, 1)
 
     # Getters and setters
