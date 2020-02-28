718aeb70e81ada522f1a97b58d0901c1fea6fe9a
Rangers <447192998@qq.com>
2020-02-29 00:02:42 +0800

    update some code

--- Rangers/try.py
+++ Rangers/try.py
@@ -2,7 +2,7 @@ from manimlib.imports import *
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 ##################### Tutorial ###################
 
@@ -2759,8 +2759,10 @@ class LaggedStartMapTest(Scene):
         )
         self.wait()
         self.play(
-            LaggedStartMap(FadeOutAndShiftUp,title),
-            LaggedStartMap(FadeOutAndShiftDown,basel),
+            LaggedStartMap(FadeOutAndShiftUp,
+                            VGroup(*[i for i in title[0]]),rate_func=there_and_back),
+            LaggedStartMap(FadeOutAndShiftDown,
+                            VGroup(*[i for i in basel[0]]),rate_func=smooth),
         )
         self.wait()
 
@@ -3111,7 +3113,7 @@ class SuccessionExample1Fail(Scene):
         self.play(
                 ShowCreationThenDestruction(
                                 dashed_line,
-                                submobject_mode="lagged_start"
+                                submobject_mode="all_at_once"
                                             ),
                 run_time=5
             )
@@ -3134,11 +3136,16 @@ class SuccessionExample1(Scene):
         self.wait(0.3)
         
         self.play(
-                    ShowCreationThenDestruction(dashed_line,submobject_mode="lagged_start",run_time=5),
-                    Succession(FadeOut, Mobject(), {"run_time" : 2.1},
-                    Write,text)
+                    LaggedStart(
+                        *[ShowCreationThenDestruction(dashed_segment)
+                        for dashed_segment in dashed_line],
+                        run_time=5
+                    ),
+                    AnimationGroup(
+                        Animation(Mobject(),run_time=2.1),
+                        Write(text),lag_ratio=1
+                    )
             )
-
         self.wait()
 
 class SuccessionExample2(Scene):
@@ -3162,14 +3169,22 @@ class SuccessionExample2(Scene):
         
         self.play(
                     ApplyMethod(triangle.shift,RIGHT*4,rate_func=linear,run_time=4),
-                    Succession(Animation, Mobject(), {"run_time" : 1},
-                    Write,text_1),
-                    Succession(Animation, Mobject(), {"run_time" : 2},
-                    Write,text_2),
-                    Succession(Animation, Mobject(), {"run_time" : 3},
-                    Write,text_3),
-                    Succession(Animation, Mobject(), {"run_time" : 4},
-                    Write,text_4)
+                    AnimationGroup(
+                        Animation(Mobject(),run_time=1),
+                        Write(text_1),lag_ratio=1
+                    ),
+                    AnimationGroup(
+                        Animation(Mobject(),run_time=2),
+                        Write(text_2),lag_ratio=1
+                    ),
+                    AnimationGroup(
+                        Animation(Mobject(),run_time=3),
+                        Write(text_3),lag_ratio=1
+                    ),
+                    AnimationGroup(
+                        Animation(Mobject(),run_time=4),
+                        Write(text_4),lag_ratio=1
+                    )
             )
 
         self.wait()
@@ -3191,13 +3206,15 @@ class SuccessionExample2Compact(Scene):
         
         self.play(
                     ApplyMethod(triangle.shift,RIGHT*4,rate_func=linear,run_time=4),
-                    *[Succession(Animation, Mobject(), {"run_time" : i+1},
-                    Write,numbers[i])for i in range(4)],
+                    *[AnimationGroup(
+                        Animation(Mobject(),run_time=i+1),
+                        Write(numbers[i]),lag_ratio=1
+                    )for i in range(4)],
             )
 
         self.wait()
 
-class SuccessionExample4Fail(Scene):
+class SuccessionExample3(Scene):
     def construct(self):
         number_line=NumberLine(x_min=-2,x_max=2)
         text_1=TextMobject("Theorem of")\
@@ -3210,12 +3227,18 @@ class SuccessionExample4Fail(Scene):
                                 color=YELLOW,
                               ).set_stroke(width=11)
 
-        self.add(number_line)
+        self.add(number_line,text_1)
         
         self.play(
-                    ShowCreationThenDestruction(dashed_line,submobject_mode="lagged_start",run_time=5),
-                    Succession(Animation,text_1, {"run_time" : 2},
-                    ReplacementTransform,text_1,text_2),
+                    LaggedStart(
+                        *[ShowCreationThenDestruction(dashed_segment)
+                        for dashed_segment in dashed_line],
+                        run_time=5
+                    ),
+                    AnimationGroup(
+                        Animation(Mobject(),run_time=2.1),
+                        ReplacementTransform(text_1,text_2),lag_ratio=1
+                    )
             )
 
         self.wait()
@@ -3949,3 +3972,228 @@ class TriangleScene(Scene):
         self.play(ShowCreation(circle,run_time=3))
 
         self.wait()
+
+######################################################################################
+
+
+class FunctionTracker(Scene):
+    def construct(self):
+        # f(x) = x**2
+        fx = lambda x: x.get_value()**2
+        # ValueTrackers definition
+        x_value = ValueTracker(0)
+        fx_value = ValueTracker(fx(x_value))
+        # DecimalNumber definition
+        x_tex = DecimalNumber(x_value.get_value()).add_updater(lambda v: v.set_value(x_value.get_value()))
+        fx_tex = DecimalNumber(fx_value.get_value()).add_updater(lambda v: v.set_value(fx(x_value)))
+        # TeX labels definition
+        x_label = TexMobject("x = ")
+        fx_label = TexMobject("x^2 = ")
+        # Grouping of labels and numbers
+        group = VGroup(x_tex,fx_tex,x_label,fx_label).scale(2.6)
+        VGroup(x_tex, fx_tex).arrange_submobjects(DOWN,buff=3)
+        # Align labels and numbers
+        x_label.next_to(x_tex,LEFT, buff=0.7,aligned_edge=x_label.get_bottom())
+        fx_label.next_to(fx_tex,LEFT, buff=0.7,aligned_edge=fx_label.get_bottom())
+
+        self.add(group.move_to(ORIGIN))
+        self.wait(3)
+        self.play(
+            x_value.set_value,30,
+            rate_func=linear,
+            run_time=10
+            )
+        self.wait()
+        self.play(
+            x_value.set_value,0,
+            rate_func=linear,
+            run_time=10
+            )
+        self.wait(3)
+
+
+class FunctionTrackerWithNumberLine(Scene):
+    def construct(self):
+        # f(x) = x**2
+        fx = lambda x: x.get_value()**2
+        # ValueTrackers definition
+        x_value = ValueTracker(0)
+        fx_value = ValueTracker(fx(x_value))
+        # DecimalNumber definition
+        x_tex = DecimalNumber(x_value.get_value()).add_updater(lambda v: v.set_value(x_value.get_value()))
+        fx_tex = DecimalNumber(fx_value.get_value()).add_updater(lambda v: v.set_value(fx(x_value)))
+        # TeX labels definition
+        x_label = TexMobject("x = ")
+        fx_label = TexMobject("x^2 = ")
+        # Grouping of labels and numbers
+        group = VGroup(x_tex,fx_tex,x_label,fx_label).scale(2)
+        # Set the labels position
+        x_label.next_to(x_tex,LEFT, buff=0.7,aligned_edge=x_label.get_bottom())
+        fx_label.next_to(fx_tex,LEFT, buff=0.7,aligned_edge=fx_label.get_bottom())
+        # Grouping numbers and labels
+        x_group = VGroup(x_label,x_tex)
+        fx_group = VGroup(fx_label,fx_tex)
+        # Align labels and numbers
+        VGroup(x_group, fx_group).arrange_submobjects(RIGHT,buff=2,aligned_edge=DOWN).to_edge(UP)
+        # Get NumberLine,Arrow and label from x
+        x_number_line_group = self.get_number_line_group(
+            "x",30,0.2,step_label=10,v_tracker=x_value,tick_frequency=2
+            )
+        x_number_line_group.to_edge(LEFT,buff=1)
+        # Get NumberLine,Arrow and label from f(x)
+        fx_number_line_group = self.get_number_line_group(
+            "x^2",900,0.012,step_label=100,v_tracker=fx_tex,
+            tick_frequency=50
+            )
+        fx_number_line_group.next_to(x_number_line_group,DOWN,buff=1).to_edge(LEFT,buff=1)
+
+        self.add(
+            x_number_line_group,
+            fx_number_line_group,
+            group
+            )
+        self.wait()
+        self.play(
+            x_value.set_value,30,
+            rate_func=linear,
+            run_time=10
+            )
+        self.wait()
+        self.play(
+            x_value.set_value,0,
+            rate_func=linear,
+            run_time=10
+            )
+        self.wait(3)
+
+
+    def get_numer_labels_to_numberline(self,number_line,x_max=None,x_min=0,buff=0.2,step_label=1,**tex_kwargs):
+        # This method return the labels of the NumberLine
+        labels = VGroup()
+        x_max = number_line.x_max
+        for x in range(x_min,x_max+1,step_label):
+            x_label = TexMobject(f"{x}",**tex_kwargs)
+            # See manimlib/mobject/number_line.py CONFIG dictionary
+            x_label.next_to(number_line.number_to_point(x),DOWN,buff=buff)
+            labels.add(x_label)
+        return labels
+
+    def get_number_line_group(self,label,x_max,unit_size,v_tracker,step_label=1,**number_line_config):
+        # Set the Label (x,or x**2)
+        number_label = TexMobject(label)
+        # Set the arrow 
+        arrow = Arrow(UP,DOWN,buff=0).set_height(0.5)
+        # Set the number_line
+        number_line = NumberLine(
+            x_min=0,
+            x_max=x_max,
+            unit_size=unit_size,
+            numbers_with_elongated_ticks=[],
+            **number_line_config
+            )
+        # Get the labels from number_line
+        labels = self.get_numer_labels_to_numberline(number_line,step_label=step_label,height=0.2)
+        # Set the arrow position
+        arrow.next_to(number_line.number_to_point(0),UP,buff=0)
+        # Grouping arrow and number_label
+        label = VGroup(arrow,number_label)
+        # Set the position of number_label
+        number_label.next_to(arrow,UP,buff=0.1)
+        # Grouping all elements
+        numer_group = VGroup(label,number_line,labels)
+        # Set the updater to the arrow and number_label
+        label.add_updater(lambda mob: mob.next_to(number_line.number_to_point(v_tracker.get_value()),UP,buff=0))
+
+        return numer_group
+
+#HSL color, see https://pypi.org/project/colour/
+def HSL(hue,saturation=1,lightness=0.5):
+    return Color(hsl=(hue,saturation,lightness))
+
+
+# This function is come and go, but linear
+def double_linear(t):
+    if t < 0.5:
+        return linear(t*2)
+    else:
+        return linear(1-(t-0.5)*2)
+
+class ValueTrackerWithColor(Scene):
+    def construct(self):
+        gradient_rectangle = Rectangle(
+                                    width=FRAME_WIDTH-1,
+                                    height=1,
+                                    fill_opacity=1,
+                                    # Gradient direction
+                                    sheen_direction=RIGHT,
+                                    stroke_width=0
+                                    )
+        square = Square(fill_opacity=1)
+        square.to_edge(UP,buff=1)
+        gradient_rectangle.to_edge(DOWN,buff=1)
+
+        gradient_rectangle.set_color(color=self.get_hsl_set_colors())
+
+        color_tracker = ValueTracker(0)
+
+        color_label = Integer(color_tracker.get_value(),unit="^\\circ")
+        color_label.add_updater(lambda v: v.set_value(color_tracker.get_value()).next_to(square,UP))
+
+        square.add_updater(lambda s: s.set_color(HSL(color_tracker.get_value()/360)))
+
+        line_color = Line(
+                        gradient_rectangle.get_corner(UL),
+                        gradient_rectangle.get_corner(UR)
+                        )
+        arrow = Arrow(LEFT,RIGHT)
+        arrow.add_updater(lambda a: a.put_start_and_end_on(square.get_bottom()+DOWN*0.3,line_color.point_from_proportion(color_tracker.get_value()/360)))
+
+        self.add(gradient_rectangle,square,color_label,arrow)
+        self.wait(3)
+        self.play(
+            color_tracker.set_value,360,
+            rate_func=double_linear,
+            run_time=20,
+            )
+        self.wait(3)
+
+    def get_hsl_set_colors(self,saturation=1,lightness=0.5):
+        return [*[HSL(i/360,saturation,lightness) for i in range(360)]]
+
+
+class MmodNTracker(Scene):
+    CONFIG = {
+        "number_of_lines": 400,
+        "gradient_colors":[RED,YELLOW,BLUE],
+        "end_value":100,
+        "total_time":180,
+    }
+    def construct(self):
+        circle = Circle().set_height(FRAME_HEIGHT*0.9)
+        mod_tracker = ValueTracker(0)
+        lines = self.get_m_mod_n_objects(circle,mod_tracker.get_value())
+        lines.add_updater(
+            lambda mob: mob.become(
+                self.get_m_mod_n_objects(circle,mod_tracker.get_value())
+                )
+            )
+        self.add(circle,lines)
+        self.wait(3)
+        self.play(
+            mod_tracker.set_value,self.end_value,
+            rate_func=linear,
+            run_time=self.total_time
+            )
+        self.wait(3)
+
+    def get_m_mod_n_objects(self,circle,x,y=None):
+        if y==None:
+            y = self.number_of_lines
+        lines = VGroup()
+        for i in range(y):
+            start_point = circle.point_from_proportion((i%y)/y)
+            end_point = circle.point_from_proportion(((i*x)%y)/y)
+            line = Line(start_point,end_point).set_stroke(width=1)
+            lines.add(line)
+        lines.set_color_by_gradient(*self.gradient_colors)
+        return lines
