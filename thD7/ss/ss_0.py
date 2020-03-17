13be3ac21e55d7c434082125c49efe5bd86fddea
Rangers <447192998@qq.com>
2020-03-17 20:04:26 +0800

    finish

--- thD7/ss/ss_0.py
+++ thD7/ss/ss_0.py
@@ -1,13 +1,6 @@
 from manimlib.imports import *
 from thD7.video_scenes import *
 
-class SS_0_Preamble(Preamble):
-    CONFIG={
-        'c_title': '序言',
-        'e_title': 'Introduction',
-        'series': r'\scriptsize Signals and Systems Series\\ \# \sz{0}',        
-    }
-
 class SS_1_Epilogue(Epilogue):
     CONFIG = {
         'bgm': [
@@ -16,8 +9,47 @@ class SS_1_Epilogue(Epilogue):
         'acknowledgement': None,
     }
 
+class Words(Scene):
+    def construct(self):
+        set_gpus([0,1])
+
+        words=[
+            '信号与系统',
+            '是众多工科的基础课程',
+            '其地位其实不亚于高数、概统和线代',
+            '在最初没有学这门课的时候',
+            '我是有一点点害怕的',
+            '但事实上其实并没有想象的那么难',
+            '但由于教材、老师等原因',
+            '很多内容的实质被掩盖在公式和计算之下',
+            '这也是我创作这个系列的原因',
+            '系列内容是基于我之前的笔记和个人的理解',
+            '当然了,还参考了很多优秀的资源',
+            '仅仅希望对观者有所帮助',
+            '这些视频是用Manim制作的',
+            '在此要特别感谢3b1b大神的开源库!',
+            '另外,与3b1b不同的是',
+            '这不是科普视频',
+            '我觉得还是挺硬核的',
+            '不过我觉得',
+            '与其说Grant Sanderson是数学的科普者',
+            '他更像是数学的布道者!'
+        ]
+
+        words_g=VGroup(*[TextMobject(i) for i in words])
+
+        self.play(Write(words_g[0]))
+        for i,k in enumerate(words_g):
+            if i == 0:
+                continue
+            self.wait(2)
+            self.play(FadeOut(words_g[i-1]),Write(k))
+
+
 class Series(Scene):
     def construct(self):
+        set_gpus([0,1])
+
         title=Title('\\LARGE Signals and Systems Series')
         self.play(Write(title))
 
